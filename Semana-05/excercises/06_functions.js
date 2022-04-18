59f68a73b5c6f396ee47b48bc3f939c654b58a88
Lucas Prat <lucasprat.cn@gmail.com>
2022-04-18 20:16:47 -0300

    BaSP-2022: ex06-Functions

--- Semana-05/excercises/06_functions.js
+++ Semana-05/excercises/06_functions.js
@@ -1,84 +1,58 @@
 ///////////////// Excercise A
 
-function suma(a = 0,b = 0) {
+function sumaA(a,b) {
     return a + b;
 }
-var resultado = suma(15,32);
-console.log("ex06-Functions-A: resultado: ",resultado);
+var resultadoA = sumaA(15,32);
+console.log("ex06-Functions-A: resultado: ",resultadoA);
 
 ///////////////// Excercise B
 
-function suma(a = 0,b = 0) {
-    if (typeof a == "number" && typeof b == "number" ) {
-        return a + b;
-    } else {
+function sumaB(a,b) {
+    if (isNaN(a) || isNaN(b)) {
         alert("ERROR uno de los parametros no es un numero");
         return NaN;
     }
+    return a + b
 }
-var resultado = suma(20,48);
-console.log("ex06-Functions-B: resultado: ",resultado);
+var resultadoB = sumaB(24,75);
+console.log("ex06-Functions-B: resultado: ",resultadoB);
 
 /////////////// Excercise C
 
 function validateInteger(x) {
-    if (x % 1 == 0) {
-        return true;
-    } else {
-        return false;
-    }
+    return  Number.isInteger(x) 
 }
-var entero = validateInteger(55.55);
+var entero = validateInteger(5566);
 console.log("ex06-Functions-C: Entero: ",entero);
 
 /////////////// Excercise D
 
-function suma(a = 0,b = 0) {
-    if (typeof a == "number" && typeof b == "number" ) {
-        if (validateInteger(a) == true && validateInteger(b) == true) {
+function validacionSuma(a = 0,b = 0) {
+    if (!isNaN(a) && !isNaN(b)) {
+        if (validateInteger(a) && validateInteger(b)) {
             return a + b;
         } else {
-            if (validateInteger(a) == false) {
-                alert("ERROR el parametro " + a + " no es entero");
-                return Math.round(a)
-            } else {
-                alert("ERROR el parametro " + b + " no es entero");
-                return Math.round(b)
-            }
-        } 
-    } else {
-        alert("ERROR uno de los parametros no es un numero");
-        return NaN;
-    }
-}
-var resultado = suma(38,25);
-console.log("ex06-Functions-D: resultado: ",resultado);
-
-/////////////// Excercise E
-
-function validacionSuma(a,b) {
-    if (typeof a == "number" && typeof b == "number" ) {
-        if (validateInteger(a) == true && validateInteger(b) == true) {
-            return true;
-        } else {
-            if (validateInteger(a) == false) {
+            if (!validateInteger(a)) {
                 alert("ERROR el parametro " + a + " no es entero");
                 return Math.round(a);
             } else {
                 alert("ERROR el parametro " + b + " no es entero");
                 return Math.round(b);
             }
-        } 
+        }
     } else {
         alert("ERROR uno de los parametros no es un numero");
         return NaN;
     }
 }
-function suma(a,b) {
-    var sum = validacionSuma(a,b);
-    if (sum == true) {
-        return a + b;
-    } else return sum;
+var resultadoD = validacionSuma(444,52);
+console.log("ex06-Functions-D: resultado: ",resultadoD);
+
+/////////////// Excercise E
+
+function sumaE(a,b) {
+    return validacionSuma(a,b);
 }
-var resultado = suma(504,2584);
-console.log("ex06-Functions-E: resultado: ",resultado);
+var resultadoE = sumaE(2452,4234);
+console.log("ex06-Functions-E: resultado: ",resultadoE);
