0313618c8865db404a2518daa0ab73dcf7ee984a
Lucas Prat <lucasprat.cn@gmail.com>
2022-04-18 00:18:27 -0300

    BaSP-2022: ex05-For

---
+++ Semana-05/excercises/05_for.js
@@ -0,0 +1,29 @@
+// Excercise A
+
+var palabras = ["auto", "moto", "barco", "avion", "bicicleta"];
+for (var index = 0; index < palabras.length; index++) {
+    console.log("ex05-For-A: palabra ", index, ":", palabras[index]);
+}
+
+// Excercise B
+
+for (var index = 0; index < palabras.length; index++) {
+    palabras[index] = palabras[index].substring(0, 1).toUpperCase() + palabras[index].substring(1);
+    console.log("ex05-For-B: palabra modificada", index, ":", palabras[index]);
+}
+
+// Excercise C
+
+var sentence = "";
+for (var index = 0; index < palabras.length; index++) {
+    var sentence = sentence + " " + palabras[index];
+}
+console.log("ex05-For-C: sentence:", sentence);
+
+// Escercise D
+
+var arrayVacio = [];
+for (let index = 0; index < 10; index++) {
+    arrayVacio[index] = index;
+}
+console.log("ex05-For-D: array completo: ",arrayVacio)
