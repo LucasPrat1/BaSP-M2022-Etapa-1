68db8dc6aa733912fbc72319c0ba64d09a41a6d1
Lucas Prat <lucasprat.cn@gmail.com>
2022-04-18 00:13:36 -0300

    BaSP-2022: ex01-variables and operators

--- Semana-05/excercises/01_variables_and_operators.js
+++ Semana-05/excercises/01_variables_and_operators.js
@@ -2,22 +2,19 @@
 
 var num1 = 10;
 var num2 = 20;
-var num3 = num1 + num2;
-
-console.log ("ex01-variables and operators-A:", num3);
+var suma = num1 + num2;
+console.log ("ex01-variables and operators-A:", suma);
 
 // Excercise B
 
 var txt1 = "Hola";
 var txt2 = "Mundo";
-var txt3 = txt1 + " " + txt2;
-
-console.log ("ex01-variables and operators-B:", txt3);
+var concat = txt1 + " " + txt2;
+console.log ("ex01-variables and operators-B:", concat);
 
 // Excercise C
 
 var st1 = "Lucas Exequiel";
 var st2 = "Prat";
-var st3 = st1.length + st2.length;
-
-console.log ("ex01-variables and operators-C:", st3);
+var sumaLenght = st1.length + st2.length;
+console.log ("ex01-variables and operators-C:", sumaLenght);
