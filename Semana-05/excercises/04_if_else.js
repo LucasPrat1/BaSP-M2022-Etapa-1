fea91172f3555f3fde269d5967655f40ba34f088
Lucas Prat <lucasprat.cn@gmail.com>
2022-04-18 00:17:17 -0300

    BaSP-2022: ex04-If else

---
+++ Semana-05/excercises/04_if_else.js
@@ -0,0 +1,27 @@
+// Excercise A
+
+var randomNum = Math.random()
+if (randomNum>=0.5) {
+    console.log("ex04-If Else-A: ",randomNum," is Greater than 0,5");
+} else {
+    console.log("ex04-If Else-A: ",randomNum," is Lower than 0,5");
+}
+
+// Excercise B
+
+var age = Math.floor(Math.random() * 100);
+if (age < 2) {
+    console.log("ex04-If Else-B: Edad: ", age ,"Es Bebe");
+} else if (age >= 2 && age <= 12) {
+    console.log("ex04-If Else-B: Edad: ", age ,"Es Niño");
+} else if (age >= 13 && age <= 19) {
+    console.log("ex04-If Else-B: Edad: ", age ,"Es Adolescente");
+} else if (age >= 20 && age <= 30) {
+    console.log("ex04-If Else-B: Edad: ", age ,"Es Joven");
+} else if (age >= 31 && age <= 60) {
+    console.log("ex04-If Else-B: Edad: ", age ,"Es Adulto");
+} else if (age >= 61 && age <= 75) {
+    console.log("ex04-If Else-B: Edad: ", age ,"Es Adulto Mayor");
+} else if (age > 75) {
+    console.log("ex04-If Else-B: Edad: ", age ,"Es Anciano");
+}
